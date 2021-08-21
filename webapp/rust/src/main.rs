bfd948f05ad821ef1eaf731c421f79233ab92639
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-08-21 17:34:41 +0900

    log 削除

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -240,8 +240,7 @@ struct JIAServiceRequest<'a> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info,sqlx=warn"))
-        .init();
+    
     let mysql_connection_env = MySQLConnectionEnv::default();
 
     let pool = sqlx::mysql::MySqlPoolOptions::new()
